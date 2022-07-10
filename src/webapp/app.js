207cd6d1a28e062c4947b852859b17308d61a5b9
jrms <jarmo.seljamaa@gmail.com>
2022-07-10 23:41:32 +0300

    Switching between posts and messages

--- src/webapp/app.js
+++ src/webapp/app.js
@@ -2,21 +2,26 @@ import { startHeaderClock } from "./header_clock.js";
 import { getJSON } from "./read_JSON.js";
 import { populatePosts } from "./populate_posts.js";
 
-export const postsElement = document.getElementById('posts');
+export const postsElement = document.querySelector('.posts');
+const messageElement = document.querySelector('.messages');
+const userElements = document.querySelectorAll('.userName');
+const messageHeader = document.querySelector('.newMessageHeader');
+const closeMessages = document.getElementById('closeButton');
 
 startHeaderClock;
 
 let postsArray = await getJSON('/src/static/postsData.json')
 populatePosts(postsArray);
 
-const userElements = document.querySelectorAll('.userName');
-
-console.log(userElements);
-
 userElements.forEach((user) => {
     user.addEventListener('click', () => {
         postsElement.classList.add('hidden');
-        console.log(user.textContent);
+        messageElement.classList.remove('hidden');
+        messageHeader.textContent = `Your conversation with ${user.textContent}`;
     });
 });
 
+closeMessages.addEventListener('click', () => {
+    postsElement.classList.remove('hidden');
+    messageElement.classList.add('hidden');
+});
