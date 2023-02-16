cb2ed4e7ff963d27257712c91c7399ea63192d2b
Jollyroger <roger.suuressaar@gmail.com>
2023-02-16 16:41:12 +0000

    need still sort the online users, smth wrong with people who havent sent msg

--- src/webapp/users.js
+++ src/webapp/users.js
@@ -17,6 +17,11 @@ export async function populateUsers() {
         offlineUsersWrapper.innerHTML = '';
         //Update usersObject with ws given list of users
 
+        //TO DO
+        //get online users from websocketusers.data.online
+        //but
+        // sort online users in the same order
+        // as usersobject.offline is sorted
         usersObject.online = webSocketUsers.data.online
 
         //sort alphabetically
@@ -27,7 +32,8 @@ export async function populateUsers() {
         // });
         console.log(usersObject.online);
         console.log(usersObject.offline);
-        usersObject.offline = createOnlineUsers(usersObject.online, usersObject.offline)
+        usersObject.online = sortOnlineUsers(usersObject.online, usersObject.offline)
+        usersObject.offline = removeDoubleUsers(usersObject.online, usersObject.offline)
         console.log(usersObject.offline);
         constructUserLists(usersObject.online, onlineUsersWrapper, 'online');
         constructUserLists(usersObject.offline, offlineUsersWrapper, 'offline');
@@ -57,7 +63,7 @@ async function loadUsersObject() {
     return data
 }
 
-const createOnlineUsers = function (onlineUsers, offlineUsers) {
+const removeDoubleUsers = function (onlineUsers, offlineUsers) {
     offlineUsers.forEach(function (user) {
         if (onlineUsers.find(e => e.name === user.name)) {
             offlineUsers = offlineUsers.filter(item => item !== user)
@@ -66,6 +72,30 @@ const createOnlineUsers = function (onlineUsers, offlineUsers) {
     return offlineUsers
 }
 
+const sortOnlineUsers = function (onlineUsers, offlineUsers) {
+
+    // Create a map of user names to their indices in the offline array
+    const offlineIndexMap = {};
+    for (let i = 0; i < offlineUsers.length; i++) {
+        const userName = offlineUsers[i].name;
+        offlineIndexMap[userName] = i;
+    }
+
+    // Sort the online array based on their indices in the offline array
+    onlineUsers.sort((a, b) => {
+        const aIndex = offlineIndexMap[a.name];
+        const bIndex = offlineIndexMap[b.name];
+        if (aIndex === undefined || bIndex === undefined) {
+            return 0; // If a user doesn't exist in the offline array, leave it in the same position
+        } else {
+            return aIndex - bIndex;
+        }
+    });
+
+    // Now the online users array should be sorted in the same order as the offline array
+    console.log(onlineUsers);
+    return onlineUsers
+}
 
 const constructUserLists = (usersArray, usersWrapper, type) => {
     let heading = createDiv(`${type}-group`, `<i class="fa-solid fa-comments"></i>${usersArray.length} users ${type}`);
